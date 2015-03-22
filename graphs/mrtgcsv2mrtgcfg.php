cb4c1efbda477f05981e34d2902954648debb97a
Ramon Roca <ramon.roca@guifi.net>
2015-03-22 17:41:27 +0100

    Refreshes the MRTG cfg file using the blacklisted ips to ban the ip's known that will not give a response

--- graphs/mrtgcsv2mrtgcfg.php
+++ graphs/mrtgcsv2mrtgcfg.php
@@ -6,57 +6,10 @@ if (file_exists("../common/config.php")) {
   include_once("../common/config.php.template");
 }
 
-$now = time();
-$mlast= @fopen("/tmp/last_mrtg", "r");
-if ($mlast)
-  $last = fgets($mlast);
-else
-  $last = 0;
-print "Last: ".date('Y/m/d H:i:s',((int)$last)+(60*60))."\n";
-print "Now: ".date('Y/m/d H:i:s',(int)$now)."\n";
-print "ServerId: ".$SNPGraphServerId."\n";
-#
-# Looks if the files has been refreshed at least between 60 an 90 mins ago
-# range 60..90 depends on $SNPGraphServerId mod 30
-# if is still fresh, does not even looks to the server to check if it has changed
-#
-$secs = $SNPGraphServerId % 285;
-$mins = $SNPGraphServerId % 30;
-$nanos = $SNPGraphServerId % 10;
-if (($last) and ($now < ($last +  ((60 + $mins) * 60)))) {
-  fclose($mlast);
-  echo "Still fresh.\n";
-  exit();
-}
+function mrtgcfg_from_mrtgcsv($rrdtool_header,$rrdimg_path,$rrddb_path,$mrtg_traffic_template,$mrtg_ping_template) {
+system('/bin/grep -vf /tmp/blacklist.ips /tmp/mrtg.csv > /tmp/mrtg.blacklisted.csv');
 
-#
-# Local file is not fresh, so looks to the server and cheks if has changed
-#
-$hlastnow = @fopen($SNPDataServer_url."/guifi/refresh/cnml", "r") or die('Error reading changes\n');
-$last_now = fgets($hlastnow);
-fclose($hlastnow);
-$hlast= @fopen("/tmp/last_update.mrtg", "r");
-if (($hlast) and ($last_now == fgets($hlast))) {
-  fclose($hlast);
-  echo "No changes.\n";
-  $hlast= @fopen("/tmp/last_mrtg", "w+") or die('Error!');
-  fwrite($hlast,$now);
-  fclose($hlast);
-  exit();
-}
-print "Sever CNML dated as: ".date('Y/m/d H:i:s',$last_now)."\n";
-
-#
-# Server CNML has changed, so going to call the server for the new file
-# Befoge calling, sleep $SNPGrahServerId mod 285 (4 min, 45 segs) to spread across that
-# timeslot.
-#
-
-print "Waiting for  ".$secs.".".$nanos." seconds\n";
-time_nanosleep($secs,($nanos * 10000000));
-print date('Y/m/d H:i:s')."\n";
-
-$hf = @fopen($MRTGConfigSource,"r") or die('Error reading MRTG csv input\n"');
+$hf = @fopen('/tmp/mrtg.blacklisted.csv','r');
 $cf = @fopen('../data/mrtg.cfg','w+');
 
 fputs($cf,sprintf($rrdtool_header,$rrdimg_path,$rrdimg_path,$rrddb_path,$rrddb_path));
@@ -107,7 +60,10 @@ while ( $buffer = fgets($hf, 4096) ) {
       $rn = $dev[0].'-'.$k;
       // snmp is given by interface name
       $d = explode(';',$r);
-      $wn = $d[1];
+      if (isset($d[1]))
+        $wn = $d[1];
+      else
+        $wn = null;
       $trap = '\\'.$d[0];
     }
     fputs($cf,sprintf($mrtg_traffic_template."\n",
@@ -131,6 +87,86 @@ while ( $buffer = fgets($hf, 4096) ) {
 }
 fclose($hf);
 fclose($cf);
+}
+
+if ($argv[1]=="CACHE") {
+  if (file_exists('/tmp/blacklist.ips')) { 
+    $blacklist_time = filemtime('/tmp/blacklist.ips');
+    if (file_exists('../data/mrtg.cfg')) {
+       $mrtg_time = filemtime('../data/mrtg.cfg');
+    } else {
+       $mrtg_time = 0;
+       echo "No mrtg.cfg file present, refreshing...\n";
+    }
+    if ($blacklist_time > $mrtg_time)
+      mrtgcfg_from_mrtgcsv($rrdtool_header,$rrdimg_path,$rrddb_path,$mrtg_traffic_template,$mrtg_ping_template);
+    else
+      echo "MRTG.cfg is uptated $blacklist_time - $mrtg_time with the current blacklist\n";
+  }
+  exit();
+}
+
+$now = time();
+$mlast= @fopen("/tmp/last_mrtg", "r");
+if ($mlast)
+  $last = fgets($mlast);
+else
+  $last = 0;
+print "Last: ".date('Y/m/d H:i:s',((int)$last)+(60*60))."\n";
+print "Now: ".date('Y/m/d H:i:s',(int)$now)."\n";
+print "ServerId: ".$SNPGraphServerId."\n";
+#
+# Looks if the files has been refreshed at least between 60 an 90 mins ago
+# range 60..90 depends on $SNPGraphServerId mod 30
+# if is still fresh, does not even looks to the server to check if it has changed
+#
+$secs = 0;
+$mins = $SNPGraphServerId % 30;
+$nanos = $SNPGraphServerId % 10;
+if (($last) and ($now < ($last +  ((60 + $mins) * 60)))) {
+  fclose($mlast);
+  echo "Still fresh.\n";
+  exit();
+}
+
+#
+# Local file is not fresh, so looks to the server and cheks if has changed
+#
+$hlastnow = @fopen($SNPDataServer_url."/guifi/refresh/cnml", "r") or die('Error reading changes\n');
+$last_now = fgets($hlastnow);
+fclose($hlastnow);
+$hlast= @fopen("/tmp/last_update.mrtg", "r");
+if (($hlast) and ($last_now == fgets($hlast))) {
+  fclose($hlast);
+  echo "No changes.\n";
+  $hlast= @fopen("/tmp/last_mrtg", "w+") or die('Error!');
+  fwrite($hlast,$now);
+  fclose($hlast);
+  exit();
+}
+print "Sever CNML dated as: ".date('Y/m/d H:i:s',$last_now)."\n";
+
+#
+# Server CNML has changed, so going to call the server for the new file
+# Befoge calling, sleep $SNPGrahServerId mod 285 (4 min, 45 segs) to spread across that
+# timeslot.
+#
+
+print "Waiting for  ".$secs.".".$nanos." seconds\n";
+time_nanosleep($secs,($nanos * 10000000));
+print date('Y/m/d H:i:s')."\n";
+
+$hf = @fopen($MRTGConfigSource,"r") or die("Error reading MRTG csv input\n");
+$af = @fopen('/tmp/mrtg.csv','w+');
+
+while (!feof($hf)) {
+  $buffer = fgets($hf, 4096);
+  fwrite($af,$buffer);
+}
+fclose($hf);
+fclose($af);
+
+mrtgcfg_from_mrtgcsv($rrdtool_header,$rrdimg_path,$rrddb_path,$mrtg_traffic_template,$mrtg_ping_template);
 
 $hlast= @fopen("/tmp/last_update.mrtg", "w+") or die('Error!');
 fwrite($hlast,$last_now);
